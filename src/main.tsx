150d37761502d378827e3c79ca8094878696609c
ShubhamWindows <shubhamcr10@gmail.com>
2026-04-26 18:02:00 +0000

    trying fix for white page issue when deploying GH PAGES 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react';
+// import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+// import App from "./App.tsx";
+import App from './App.tsx';
+// import "./index.css";
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
